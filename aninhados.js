91eab5244bd2ec5ea2f1241c0009ee0b82ab93cf
Leonardo <leonardospfc_@hotmail.com>
2022-12-14 11:25:36 -0300

    aula 2

---
+++ aninhados.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1115555550","1144444440"]
+};
+
+
+
+cliente.endereço = {
+    rua: "Rua Jose",
+    numero: "45",
+    apartamento: true,
+    complemento: "34"
+}
+
+console.log(cliente.endereço)
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1115555550","1144444440"]
+};
+
+console.log(cliente)
